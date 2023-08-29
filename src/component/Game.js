386e99143bfec2f7e4809676030206c605656113
RukeshB <rukeshbasukala@gmail.com>
2023-08-29 20:38:01 +0545

    move win logic to game logic file

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,5 +1,6 @@
 import Board from "./Board";
 import { useEffect, useState } from "react";
+import { checkWinner } from "./GameLogic";
 
 const Game = () => {
   const [input, setInput] = useState(Array(9).fill(null));
@@ -19,29 +20,6 @@ const Game = () => {
     setXTurn((prevState) => {
       return !prevState;
     });
-  };
-
-  const checkWinner = (value) => {
-    const winLogic = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let logic of winLogic) {
-      const [a, b, c] = logic;
-
-      if (value[a] && value[a] === value[b] && value[a] === value[c]) {
-        return value[a];
-      }
-    }
-
-    return null;
   };
 
   useEffect(() => {
